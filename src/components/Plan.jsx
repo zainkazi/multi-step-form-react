f1b2e84f8dee22e732e9d7b238039c8a130bfd0a
Zain Kazi <zainkazi27@gmail.com>
2023-02-22 22:13:29 +0500

    fixed minor bugs

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -11,6 +11,7 @@ const Plan = ({
   planDuration,
 }) => {
   const [errorDisplay, setErrorDisplay] = useState("invisible");
+  const [check, setCheck] = useState(false);
 
   useEffect(() => {
     if (isPlanEmpty) {
@@ -18,7 +19,13 @@ const Plan = ({
     } else {
       setErrorDisplay("invisible");
     }
-  }, [isPlanEmpty]);
+
+    if (planDuration == "mo") {
+      setCheck(false);
+    } else {
+      setCheck(true);
+    }
+  }, [isPlanEmpty, planDuration]);
 
   return (
     <div>
@@ -45,16 +52,19 @@ const Plan = ({
       >
         Please select a plan!
       </div>
-      <div className="font-medium mt-12 bg-[#fafbff] text-[#02295a] p-2 rounded-xl">
+      <div className="font-medium mt-8 bg-[#fafbff] text-[#02295a] p-2 rounded-xl">
         <div className="flex justify-center items-center space-x-5 text-[14px]">
           <div>Monthly</div>
           <div className="scale-75">
             <label className="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div
-                onClick={onToggleDuration}
-                className="border-2 border-black w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-800"
-              ></div>
+              <input
+                onChange={onToggleDuration}
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                checked={check}
+              />
+              <div className="border-2 border-black w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-800"></div>
             </label>
           </div>
           <div>Yearly</div>
